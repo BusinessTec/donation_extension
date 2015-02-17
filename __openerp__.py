fe9d4258702df987a5925470d170ad66fd7636df
Business Tec Systems <BusinessTec@users.noreply.github.com>
2015-02-17 12:07:19 -0600

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Customer product info",
+    "version": "1.0",
+    "author": "Business Tec Systems",
+    "category": "Sales",
+    "description": """
+Adds several product information relavant to a cleint. Based on idea in Vauxoo Product Customer Code
+     """,
+    "website": "http://www.businesstec.net/",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "product",
+        "sale"
+    ],
+    "demo": [],
+    "data": [
+        "customer_product_info_view.xml"
+    ],
+    #"test": [], 
+    #"js": [], 
+    #"css": [], 
+    #"qweb": [], 
+    "installable": True,
+    "auto_install": False,
+    "active": False
+}
