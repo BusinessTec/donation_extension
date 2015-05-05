7ea051c67c28fcee8dcd085e063f219db92f961e
Business Tec Systems <BusinessTec@users.noreply.github.com>
2015-05-04 21:51:53 -0600

    Update donation.py

--- donation.py
+++ donation.py
@@ -8,4 +8,12 @@ class donation_extra(models.Model):
         _inherit = 'donation.donation'
         
         notes = fields.Text(string='Notes','Additional Information')
-        internal_number = fields.Char(string='Invoice Number', readonly=True, default=False, copy=False, )
+        donation_number = fields.Char(string='Invoice Number', readonly=True, default=False, copy=False, compute="_new_number")
+        
+        @api.one
+	@api.depends('state')
+	def _new_number(self):
+	        if self.state=='done':
+	                get_number=
+	                self.donation_number=
+	
