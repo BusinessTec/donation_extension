7b87d186a6d745fe2673f5cb61b40400c1c07509
Business Tec Systems <BusinessTec@users.noreply.github.com>
2015-05-12 11:24:36 -0600

    Update donation.py

--- donation.py
+++ donation.py
@@ -2,7 +2,6 @@
 
 from openerp import models, fields, api
 
-
 class donation_extra(models.Model):
 
         _inherit = 'donation.donation'
@@ -11,9 +10,8 @@ class donation_extra(models.Model):
         donation_number = fields.Char(string='Invoice Number', readonly=True, default=False, copy=False, compute="_new_number")
         
         @api.one
-	@api.depends('state')
+	@api.depends('id')
 	def _new_number(self):
 	        if self.state=='done':
-	                get_number=
-	                self.donation_number=
+	                self.donation_number=context_today(self)[:4]+'/'+{:04}".format(self.id)
 	
